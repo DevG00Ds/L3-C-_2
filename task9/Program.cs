a8b12293367a3a18be849542d6477343e96592f6
DevG00Ds <semenov.dmitriy.of@gmail.com>
2023-02-14 20:26:23 +0300

    change task9

--- task9/Program.cs
+++ task9/Program.cs
@@ -1,9 +1,27 @@
-﻿
+﻿// Напишите программу, которая выводит случайно число из отрезка [10: 99]
 
 //Например :
-
-
-
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
 
 int number = Random.Shared.Next(10, 100);
 Console.WriteLine(number);
+int lastDigit = number % 10;
+// Console.WriteLine(lastDigit);
+int firstDigit = number / 10;
+if ( firstDigit > lastDigit)
+{
+    // Console.WriteLine("max ");
+    // Console.WriteLine(firstDigit);
+    // Console.WriteLine("min ");
+    // Console.WriteLine(lastDigit);
+    // Console.WriteLine("max " + firstDigit + "min " + lastDigit);
+    Console.WriteLine($"max {firstDigit}, min {lastDigit}");
+}
+else
+{
+    // Console.WriteLine(lastDigit);
+    Console.WriteLine($"max {lastDigit}, min {firstDigit}");
+}
+
