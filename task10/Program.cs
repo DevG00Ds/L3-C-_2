558b9b016d876b6f758514cbee59b2ecf582eff2
DevG00Ds <semenov.dmitriy.of@gmail.com>
2023-02-14 20:59:18 +0300

    added task10

---
+++ task10/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+
+int num = Random.Shared.Next(100, 1000);
+Console.WriteLine(num);
+
+int last2 = num % 10;
+int last = num / 100;
+int res = last2*10 + last;
+
+Console.WriteLine(res);
+
+
+
+
