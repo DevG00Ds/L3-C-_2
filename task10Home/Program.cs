77fa2482469d013d83f1c5f7fc7fd8d391ae2fff
DevG00Ds <semenov.dmitriy.of@gmail.com>
2023-02-16 23:46:44 +0300

    added task10Home

---
+++ task10Home/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.WriteLine(" Введите трехзначное число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1000 && number > 100)
+{
+    int a = number / 100;
+    int b = (number / 10) % 10;
+    int c = number % 10;
+    Console.WriteLine(b);
+}
+else
+{
+    Console.WriteLine(" Число не трехзначное");
+}
