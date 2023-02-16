a16627a41f5e097cc64f88d759f9cdce617c9804
DevG00Ds <semenov.dmitriy.of@gmail.com>
2023-02-16 21:16:48 +0300

    added project task12

---
+++ task12/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет,
+// кратно ли оно одновременно 7 и 23 
+
+// 14 -> Нет 
+// 46 -> Нет 
+// 161 -> ДА 
+
+
+Console.WriteLine(" Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine(" ДА");
+}
+else
+{
+    Console.WriteLine(" НЕТ ");
+}
