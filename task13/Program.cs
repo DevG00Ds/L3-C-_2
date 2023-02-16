a62e569b2ea51561bdfcffb188e5f7a4c6ea55c4
DevG00Ds <semenov.dmitriy.of@gmail.com>
2023-02-16 21:29:28 +0300

    added project task13

---
+++ task13/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет,
+// является ли одно число квадратом другова 
+
+// 5,24 -> ДА 
+// -4,16 -> ДА
+// 25,5 -> ДА
+// 8,9 -> НЕТ
+
+
+Console.WriteLine(" Введите первое число : ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(" Введите второе число : ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if ( number1 == number2*number2 || number2 == number1*number1)
+{
+    Console.WriteLine(" Одно число является квадратом другого");
+}
+else
+{
+    Console.WriteLine(" Не является квадратом другого");
+}
