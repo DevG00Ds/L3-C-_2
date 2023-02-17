61a4e7734666f92a2c8ef56c48431f49a094d672
DevG00Ds <semenov.dmitriy.of@gmail.com>
2023-02-17 21:37:10 +0300

    add task13Home

--- task13Home/Program.cs
+++ task13Home/Program.cs
@@ -6,3 +6,18 @@
 // 32679 -> 6
 
 
+Console.WriteLine(" Введите число от 100");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100)
+{
+    Console.WriteLine(" Error");
+}
+else
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    Console.WriteLine(number % 10);
+}
